fd1460cca8d65ddd17e1ba4c2ce36c28e12f596f
ashish <ashish@airtkt.com>
2024-01-08 16:29:45 +0530

    minor changes in header section

--- src/componemts/pages/Header.jsx
+++ src/componemts/pages/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   AiOutlineSearch,
@@ -6,23 +6,69 @@ import {
   AiOutlineUser,
 } from "react-icons/ai";
 
+import dropdownMenu from "../data/dropdown.json";
+
 export default function Header() {
+  const [isSticky, setIsSticky] = useState(true);
+  const [isdropdownMenu, setisdropdownMenu] = useState(dropdownMenu);
+
+  const [formUserInfo, setFormUserInfo] = useState(false);
+  useEffect(() => {
+    const handleIsSticky = () =>
+      window.scrollY >= 50 ? setIsSticky(true) : setIsSticky(false);
+
+    window.addEventListener("scroll", handleIsSticky);
+
+    return () => {
+      window.removeEventListener("scroll", handleIsSticky);
+    };
+  }, [isSticky]);
+
+  console.log(isdropdownMenu);
   return (
-    <header id="header">
+    <div className={`header ${isSticky ? "sticky" : ""}`}>
       <div className="container">
-        <div className="logo">
-          <Link to="/">Shop</Link>
-        </div>
+        <div className="menu">
+          <div className="logo">
+            <Link to="/">Shop</Link>
+          </div>
 
-        <div className="search_field">
-          <AiOutlineSearch />
-        </div>
+          <div className="user_actions">
+            <ul>
+              <li>
+                <AiOutlineSearch />
+              </li>
+              <li>
+                <AiOutlineShoppingCart />
+              </li>
+              <li>
+                <AiOutlineUser />
+                <div className="dropdownwrapper">
+                  <p>Access account and manage orders</p>
 
-        <div className="user_actions">
-          <AiOutlineShoppingCart />
-          <AiOutlineUser />
+                  <div className="dividerline"></div>
+
+                  {!formUserInfo && (
+                    <button
+                      type="button"
+                      // onClick={() => toggleForm(true)}
+                    >
+                      Login / Signup
+                    </button>
+                  )}
+                  <ul>
+                    {isdropdownMenu.map((item) => (
+                      <li key={item.id}>
+                        <Link to={item.path}>{item.link}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
--- src/componemts/routes/RouterRoutes.jsx
+++ src/componemts/routes/RouterRoutes.jsx
@@ -1,14 +1,27 @@
-import { Routes, Route } from 'react-router';
-import Home from '../pages/Home';
-import ErrorPage from '../pages/ErrorPage';
-import Header from '../pages/Header';
+import { Routes, Route } from "react-router";
+import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
+import Header from "../pages/Header";
 export default function RouterRoutes() {
   return (
     <Routes>
-
-        <Route path="/" element={<><Home/></>} />
-        <Route path="/Header" element={<><Header/></>} />
-        <Route path="*" element={<ErrorPage />} />
+      <Route
+        path="/"
+        element={
+          <>
+            <Home />
+          </>
+        }
+      />
+      <Route
+        path="/Header"
+        element={
+          <>
+            <Header />
+          </>
+        }
+      />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
-  )
+  );
 }
